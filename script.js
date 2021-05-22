7f643fab453c39d6da370eb4c607a5339ba890f5
SpudStar <ed504@sky.com>
2021-05-22 23:23:16 +0000

    Added a method to check how many mines each tile is adjacent to

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let dimension = 10;
-let bombsNo = 0;
+let bombsNo = 20;
 let tileArray = new Array();
 
 function newGame(){
@@ -30,16 +30,72 @@ function newGame(){
     }
 
     /* Set Tiles as bombs */
-    tileArray[0].bomb = true;
-    tileArray[15].bomb = true;
-    
+    let bombsPlaced = 0;
+    do{
+        let bombPlacement = Math.floor((Math.random() * (dimension*dimension))+0);
+        if(!tileArray[bombPlacement].bomb){
+            tileArray[bombPlacement].bomb = true;
+            bombsPlaced += 1;
+        }
+    }while(bombsPlaced<bombsNo);
+
+    /*Checks how many bombs a tile is adjacent to*/
+    for(let i = 0; i < dimension*dimension; i++){
+        let adjacentBombs = 0;
+
+        for(let j = -dimension-1; j <= dimension+1; j++){
+            if((j == -dimension-1 || j == -dimension || j == -dimension+1 || j == -1 || j == 1 || j == dimension -1 || j == dimension || j == dimension + 1) && checkPossible(i,j)){
+                if(tileArray[i+j].bomb){
+                    adjacentBombs += 1;
+                }
+            }
+        }
+        tileArray[i].adjacency = adjacentBombs;
+    }
 }
 
 function revealTile(tileElement,location){
-    if(tileArray[location].bomb == true){
+    if(tileArray[location].bomb){
         tileElement.style.backgroundColor = "red";
     }
     else{
-        tileElement.style.backgroundColor = "yellow";
+        switch (tileArray[location].adjacency){
+            case 1:
+                tileElement.style.backgroundColor = "yellow";
+            break;
+            case 2:
+                tileElement.style.backgroundColor = "green";
+            break;
+            case 3:
+                tileElement.style.backgroundColor = "blue";
+            break;
+            case 4:
+                tileElement.style.backgroundColor = "cyan";
+            break;
+            case 5:
+                tileElement.style.backgroundColor = "orange";
+            break;
+            case 6:
+                tileElement.style.backgroundColor = "black";
+            break;
+            case 7:
+                tileElement.style.backgroundColor = "brown";
+            break;
+            case 8:
+                tileElement.style.backgroundColor = "pink";
+            break;
+            default:
+                tileElement.style.backgroundColor = "grey";
+            break;
+        }
     }
+}
+
+function checkPossible(i,j){
+    sum = i + j;
+    if(sum < 0 || sum >= dimension*dimension){
+        return false;
+    }
+
+    return true;
 }
