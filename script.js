73d7504d8e446290487fb55e4dfb205b728d070b
SpudStar <ed504@sky.com>
2021-05-22 21:01:00 +0000

    Changed to divs to allow better css control. Still vertical white space

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ function newGame(){
     for(let x=0; x<dimension; x++){
         for(let y=0; y<dimension; y++){
             let mineElement = document.createElement("button");
-            mineElement.innerHTML = "11";
-            mineElement.classList.add("my-class");
-            gameArea.appendChild(mineElement);
+            let tileElement = document.createElement("div");
+            tileElement.classList.add("game-tile");
+            /*tileElement.appendChild(mineElement);*/
+            gameArea.appendChild(tileElement);
+
+            
         }
     }
 }
