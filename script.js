34bd216fa6bc614b8f3fa793e812921363e35664
SpudStar <ed504@sky.com>
2021-05-22 22:35:56 +0000

    Changed code so a bomb can be placed at a square of my choosing, tiles update to reflect this when clicked

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let dimension = 10;
+let bombsNo = 0;
+let tileArray = new Array();
 
 function newGame(){
     let gameArea = document.getElementById("minefield-area");
@@ -7,16 +9,37 @@ function newGame(){
         gameArea.removeChild(gameArea.firstChild);
     }
 
+    var tileCount = 0;
     for(let x=0; x<dimension; x++){
         for(let y=0; y<dimension; y++){
+
+            let location = tileCount;
+
             let tileElement = document.createElement("div");
             tileElement.classList.add("game-tile");
-            tileElement.onclick = function() {revealTile(tileElement)};
+            tileElement.onclick = function() {revealTile(tileElement,location)};
             gameArea.appendChild(tileElement);
+
+            tileArray[tileCount] = {
+                adjacency: 0,
+                bomb: false
+            }
+
+            tileCount += 1;
         }
     }
+
+    /* Set Tiles as bombs */
+    tileArray[0].bomb = true;
+    tileArray[15].bomb = true;
+    
 }
 
-function revealTile(tileElement){
-    tileElement.style.backgroundColor = "red";
+function revealTile(tileElement,location){
+    if(tileArray[location].bomb == true){
+        tileElement.style.backgroundColor = "red";
+    }
+    else{
+        tileElement.style.backgroundColor = "yellow";
+    }
 }
