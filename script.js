dbd7da6bfc6bc6433964e9c3c42cca5c07946daf
SpudStar <ed504@sky.com>
2021-05-22 21:13:45 +0000

    Added code to remove the existing game, if one exists

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ let dimension = 10;
 
 function newGame(){
     let gameArea = document.getElementById("minefield-area");
-    
+
+    while (gameArea.firstChild) {
+        gameArea.removeChild(gameArea.firstChild);
+    }
+
     for(let x=0; x<dimension; x++){
         for(let y=0; y<dimension; y++){
-            let mineElement = document.createElement("button");
             let tileElement = document.createElement("div");
             tileElement.classList.add("game-tile");
-            /*tileElement.appendChild(mineElement);*/
             gameArea.appendChild(tileElement);
-
-            
         }
     }
+}
+
+function revealTile(){
+    
 }
